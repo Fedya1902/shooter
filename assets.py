82832ac79f4f94ca31a6558f41d9f50ff8cf20a1
Fedya1902 <martynenkofeda4@gmail.com>
2025-03-16 13:20:04 +0200

    Add files via upload

---
+++ assets.py
@@ -0,0 +1,9 @@
+GAME_BG_IMG = "assets/satai.jpg"
+
+ROKCET_IMG = "assets/krutoisvin.png"
+
+ENEMY_IMG = "assets/krutoismailik.png"
+
+GAME_MUSIC = "assets/Omfg Hello.mp3"
+
+FIRE_SOUND = "assets/vistrel.mp3"
