c8b644ff6b3bd2130edb3186b2f5b2330009e095
Fedya1902 <martynenkofeda4@gmail.com>
2025-03-30 13:18:11 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -7,25 +7,22 @@ mixer.init()
 mixer.music.load(GAME_MUSIC)
 mixer.music.play(-1)
 fire_sound = mixer.Sound(FIRE_SOUND)
-
-damage_sound = mixer.Sound(GAMAGE_SOUND)
-
+damage_sound = mixer.Sound(DAMAGE_SOUND)
 
 # нам потрібні такі картинки:
 img_back = GAME_BG_IMG  # фон гри
 img_hero = ROCKET_IMG  # герой
 img_enemy = ENEMY_IMG # ворог
+img_bullet = BULLET_IMG # das war ein befehl die 
 
 font.init()
-font2 = font.Font(None, 36)
-
-font1 = font.Font(None, 80)
-win = font1.render("YOU WIN!", True, (255, 255, 255))
-lose = font1.render("YOU LOSE!", True, (180, 0, 0))
-
+font2 = font.Font(None,36)
+font1 = font.Font(None,80)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180,0,0))
 score = 0
 lost = 0
-
+# клас-батько для інших спрайтів
 class GameSprite(sprite.Sprite):
     # конструктор класу
     def __init__(self, player_image, player_x, player_y, 
@@ -44,79 +41,64 @@ class GameSprite(sprite.Sprite):
     # метод, що малює героя на вікні
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-
 # клас головного гравця
 class Player(GameSprite):
-
-    # метод для керування спрайтом стрілками клавіатури
     def update(self):
         keys = key.get_pressed()
         if keys[K_LEFT] and self.rect.x > 5:
             self.rect.x -= self.speed
         if keys[K_RIGHT] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
-
-    # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
     def fire(self):
-        pass
-
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 60, 80, -15)
+        bullets.add(bullet)
 class Enemy(GameSprite):
     def update(self):
         self.rect.y += self.speed
-        global lost  
+        global lost
         if self.rect.y > win_height:
-            self.rect.x = randint(80, win_height - 80)
+            self.rect.x = randint(80, win_width - 80)
             self.rect.y = 0
-            lost = lost + 1 
-
-        
-
-
-# клас-батько для інших спрайтів
-
-# клас головного гравця
-class Player(GameSprite):
+            lost = lost + 1
+class Bullet(GameSprite):
     def update(self):
-        keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-    def fire(self):
-        pass
+        self.rect.y += self.speed
+        if self.rect.y<0: self.kill()
+
 win_width = 700
 win_height = 500
-display.set_caption("Shooter")
+display.set_caption("Likarnya")
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), 
                              (win_width, win_height))
 
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
-
 monsters = sprite.Group()
-for i in range(1, 6):
-    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+for i in range(1,6):
+    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1,5))
     monsters.add(monster)
-
+bullets = sprite.Group()
 finish = False
 run = True  # прапорець скидається кнопкою закриття вікна
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
     if not finish:
         window.blit(background, (0, 0))
-        text = font2.render("Рахунок: " + str(score), 1, (255, 255, 255))
-        window.blit(text, (10, 20))
-        text_lose = font2.render("Пропущенно: " + str(lost), 1, (255, 255, 255))
-        window.blit(text_lose, (10, 50))
+        text = font2.render("Рахунок: " + str(score), 1, (255,255,255))
+        window.blit(text, (10,20))
+        text_lose = font2.render("Пропущено: " + str(lost), 1, (255,255,255))
+        window.blit(text_lose, (10,50))
         ship.update()
         monsters.update()
-        monsters.draw(window)
         ship.reset()
+        monsters.draw(window)
+        bullets.draw(window)
+        bullets.update()
         display.update()
-        
-        
-        
-        
     time.delay(50)
